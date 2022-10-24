5ed4b69c2cd5eac99555dc912735865942785ab3
santosh <santoshmadam81@gmail.com>
2022-10-24 22:05:40 +0530

    Good Night

--- Store/src/Div.java
+++ Store/src/Div.java
@@ -1,7 +1,47 @@
-public class Div {
- 
- public static void main(String[] args) {
-  System.out.println(50/3);
- }
- 
+public class Div{
+	private  int age;
+	private  int age1;
+	private int age2;
+	private  int age3;
+	private int age4;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAge1() {
+		return age1;
+	}
+	public void setAge1(int age1) {
+		this.age1 = age1;
+	}
+	public int getAge2() {
+		return age2;
+	}
+	public void setAge2(int age2) {
+		this.age2 = age2;
+	}
+	public int getAge3() {
+		return age3;
+	}
+	public void setAge3(int age3) {
+		this.age3 = age3;
+	}
+	public int getAge4() {
+		return age4;
+	}
+	public void setAge4(int age4) {
+		this.age4 = age4;
+	}
+public static void main(String[] args) {
+	Div v =new Div();
+	v.setAge(12);
+	v.setAge1(13);
+	v.setAge2(15);
+	v.setAge3(16);
+	v.setAge4(16);
+	
+}	
 }
+
